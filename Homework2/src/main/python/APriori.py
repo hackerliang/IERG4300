bfdc4f4a73c3918cea702c9775a9b200b2e50093
Junru Zhong <billzhonggz@outlook.com>
2019-10-14 21:21:33 +0800

    Finish read baskets function.

--- Homework2/src/main/python/APriori.py
+++ Homework2/src/main/python/APriori.py
@@ -3,12 +3,26 @@ This file implements the A-Priori algorithm for finding similar item sets.
 A part of Homework #2, IERG4300, CUHK, S1 2019-2020.
 """
 
+import glob
+
 def read_baskets(path):
     """Read baskets from files.
     @param: path to data, str.
     @return: baskets, 2D list.
     """
-    pass
+    # Format of the file:
+    # One line -> one basket (word separated by \t)
+    # One line has 0 - 40 words
+    baskets = []
+    for filename in glob.glob(path + '\\*'):
+        with open(filename, 'r', encoding='utf8') as f:
+            lines = [line.rstrip('\n') for line in f]
+            # For each line, divide the words.
+            for line in lines:
+                words = line.split()
+                baskets += [words]
+    return baskets
+
 
 def freq_items(baskets, threshold):
     """Find the individual items appear frequently.
@@ -17,6 +31,7 @@ def freq_items(baskets, threshold):
     @return: frequent individual items with counts, dict.
     """
     pass
+
 
 def freq_pairs(baskets, freq_items, threshold):
     """Find the frequent item pairs.
@@ -27,7 +42,8 @@ def freq_pairs(baskets, freq_items, threshold):
     """
     pass
 
+
 if __name__ == '__main__':
-    baskets = read_baskets('D:\\Datasets\\shakespeare_basket')
-    freq_items = freq_items(baskets, 1)
-    freq_pairs = freq_pairs(baskets, freq_items, 1)
+    baskets = read_baskets('D:\\Datasets\\shakespeare_basket\\test')
+    # freq_items = freq_items(baskets, 1)
+    # freq_pairs = freq_pairs(baskets, freq_items, 1)
