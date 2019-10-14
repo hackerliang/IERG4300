d111488f392a483bc8c202a2734a573f95f60b2b
Junru Zhong <billzhonggz@outlook.com>
2019-10-14 20:36:22 +0800

    Format.

--- Homework2/src/main/python/APriori.py
+++ Homework2/src/main/python/APriori.py
@@ -3,12 +3,14 @@ This file implements the A-Priori algorithm for finding similar item sets.
 A part of Homework #2, IERG4300, CUHK, S1 2019-2020.
 """
 
+
 def read_baskets(path):
     """Read baskets from files.
     @param: path to data, str.
     @return: baskets, 2D list.
     """
     pass
+
 
 def freq_items(baskets, threshold):
     """Find the individual items appear frequently.
@@ -18,6 +20,7 @@ def freq_items(baskets, threshold):
     """
     pass
 
+
 def freq_pairs(baskets, freq_items, threshold):
     """Find the frequent item pairs.
     @param: baskets, 2D list.
@@ -26,6 +29,7 @@ def freq_pairs(baskets, freq_items, threshold):
     @return: frequent pairs with counts, dict.
     """
     pass
+
 
 if __name__ == '__main__':
     baskets = read_baskets('D:\\Datasets\\shakespeare_basket')
