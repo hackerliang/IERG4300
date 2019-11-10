4d67605e4a37dec95af6f2fc55ba239a45349eb7
Junru Zhong <billzhonggz@outlook.com>
2019-11-10 11:51:04 +0800

    Move inspect result to driver file, integrate with the procedure.

--- Homework3/k-means_driver.py
+++ Homework3/k-means_driver.py
@@ -1,6 +1,8 @@
 import os
 import logging
+import sys
 
+import matplotlib.pyplot as plt
 import numpy as np
 import boto3
 from botocore.exceptions import ClientError
@@ -59,6 +61,7 @@ def get_new_centroids(iteration_id):
     bucket_name = 'junru-big-data'
     object_name = 'mnist/output/{}/part-00000'.format(iteration_id)
     # Retrieve the object
+    logging.info('Downloading result from S3.')
     stream = get_object(bucket_name, object_name)
     if stream is not None:
         # Read first chunk of the object's contents into memory as bytes
@@ -66,18 +69,59 @@ def get_new_centroids(iteration_id):
     # Save to file.
     with open('centroids/centroids_{}.txt'.format(iteration_id), 'w') as f:
         f.writelines(new_result)
-    return new_result
+
+
+def inspect_result(iteration_id):
+    # Open training images.
+    with open('train_images.txt', 'r') as f:
+        data = []
+        for line in f:
+            data += [np.asarray(line.strip().split(','), dtype=float)]
+    # Load centroids.
+    with open('centroids/centroids_{}.txt'.format(iteration_id), 'r') as f:
+        centroids = {}
+        for line in f.readlines():
+            idx, centroids_str = line.strip().split('\t')
+            centroids[int(idx)] = [np.asarray(centroids_str.strip()[1:-1].split(', '), dtype=float), 0]
+    # Assign centroids.
+    # For each data point, calculate the distance between all centroids.
+    distance_sum = 0
+    for item in data:
+        distances = {}
+        for idx, centroid_count in centroids.items():
+            # Calculate L2 Norm.
+            distances[idx] = np.linalg.norm(centroid_count[0] - item)
+        # Get the index of the cluster with minimum distance.
+        cluster_id = min(distances, key=distances.get)
+        distance_sum += distances[cluster_id]
+        # Add to count.
+        centroids[cluster_id][1] += 1
+    return centroids, distance_sum
 
 
 def iteration(num_iterations):
+    distance_sums = []
+    distance_sums_iter = []
     for i in range(1, num_iterations + 1):
         logging.info('Starting iteration {}.'.format(i))
         run_mrjob(i)
         get_new_centroids(i)
         logging.info('Finished iteration {}'.format(i))
+        # Inspect result for each 5 steps.
+        if i % 1 == 0:
+            logging.info('Inspecting results')
+            centroids_count, distance_sum = inspect_result(i)
+            distance_sums += [distance_sum]
+            distance_sums_iter += [i]
+            logging.info('Finished inspecting result.')
+    # Plot a graph for distance sums.
+    plt.plot(distance_sums_iter, distance_sums)
+    plt.xlabel('Iteration')
+    plt.ylabel('Sum of Distances')
+    plt.savefig('sum_of_distances_plot.png')
 
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.DEBUG)
+    logging.basicConfig(level=logging.INFO)
     generate_random_centroids()
-    iteration(10)
+    iteration(int(sys.argv[1]))
